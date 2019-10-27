8f146a9e1d43d286ff855cd8fc654098a46c3a2d
EINNNA <annie_t_@hotmail.com>
2019-10-27 17:40:00 +1100

    updated data files and questions

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+        "name": "Rose",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/4/4e/Rose.jpg/revision/latest?cb=20100310220523",
+        "scores": [3,5,4,4,2,1,1,5]
+    }
+    {
+        "name": "Garbage",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/6/6d/Screenshot_2019-02-02_at_5.47.00_PM.png/revision/latest?cb=20190202224731",
+        "scores": [5,3,1,1,1,5,3,3]
+    }
+    {
+        "name": "Jim",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/f/f1/Asianjim.jpg/revision/latest/scale-to-width-down/700?cb=20190530022319",
+        "scores": [4,3,3,1,3,2,3,5]
+    }
+    {
+        "name": "Hunter",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/2/2a/Hunter.jpg/revision/latest?cb=20070928215430",
+        "scores": [2,5,3,2,5,2,5,4]
+    }
+    {
+        "name": "Jessica",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/1/1b/Jessica.jpg/revision/latest?cb=20180514102149",
+        "scores": [5,5,5,5,5,2,3,5]
+    }
+    {
+        "name": "Julie",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Julie.--File-Julie.png-thumb-250px--/revision/latest?cb=20100330201743",
+        "scores": [3,5,5,5,3,3,3,3]
+    }
+    {
+        "name": "Luke",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/6/63/Look_Koupir.png/revision/latest?cb=20101016000339",
+        "scores": [5,5,5,4,5,3,1,4]
+    }
+    {
+        "name": "Elizabeth",
+        "photo": "https://vignette.wikia.nocookie.net/theoffice/images/2/26/Elizabeth.jpg/revision/latest?cb=20070203172942",
+        "scores": [3,4,5,5,5,1,1,2]
+    }
+
+];
+
+module.exports = friends;
