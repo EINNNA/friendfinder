c6adbdfb6bacd2860f221d640e85520cfdfcec0f
EINNNA <annie_t_@hotmail.com>
2019-12-28 00:27:30 +1100

    made a bit nicer and fixed code a bit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,26 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(request,res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (request, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req,res){
-        console.log("friends submitted data", req.body)
-        var bestFriend = {
-            "name": "",
-            "photo": "",
-            "diff": 200
-        };
-
+    app.post("/api/friends", function (req, res) {
         var userData = req.body;
-        var userScores = userData.scores;
+        var userScores = userData.scores[j];
+        var bestFriendScore = 0;
         var totalDifference;
-        friends.push(userData);
-
-        console.log(userData);
-        console.log(userScores)
 
         for (i = 0; i < friends.length; i++) {
-            let friendFriend = friends[i];
-            difference = 0;
-
-            for (j = 0; j < friendFriend.scores.length; j++){
-                var userScore = userScores[j];
-                var friendScore = friendFriend.scores[j];
-
-                totalDifference += Math.abs(parseInt(userScore) - parseInt(friendScore));
+            totalDifference = 0;
+            for (j = 0; j < userScores.length; j++) {
+                totalDifference += Math.abs(parseInt(friends.score[i]) - parseInt(userScores));
             }
-            if (totalDifference <= bestFriend.diff){
-                bestFriend.name = friendFriend.name;
-                bestFriend.photo = friendFriend.photo;
-                bestFriend.diff = totalDifference;
-            }
-        }
+            if (totalDifference === bestFriendScore) {
+
+            };
+        };
         friends.push(userData);
-
         res.json(bestFriend);
-    })
-}
+    });
+};
